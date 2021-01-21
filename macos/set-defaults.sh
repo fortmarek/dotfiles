7296096c33e86df8690cb7c6d0b04fb746cc850f
fortmarek <marekfort@me.com>
2021-01-21 22:26:25 +0100

    Spped up of dock animation.

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -39,3 +39,6 @@ defaults write com.apple.Safari IncludeDevelopMenu -bool true
 defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
 defaults write com.apple.Safari "com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled" -bool true
 defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
+
+# Speed-up animation of dock
+defaults write com.apple.dock autohide-time-modifier -int 0.2;killall Dock
